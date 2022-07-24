fd603e345e8859191a1311c53aed02975321dd96
jan1720222 <97850205+jan1720222@users.noreply.github.com>
2022-07-24 13:36:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 PICS = (environ.get('PICS', 'https://i.imgur.com/RjFZcxe.jpg https://i.imgur.com/RRjBoqF.jpg https://i.imgur.com/hdsXwi8.jpg https://i.imgur.com/G8jDBxd.jpg https://i.imgur.com/oHZvutA.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '718967870').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
